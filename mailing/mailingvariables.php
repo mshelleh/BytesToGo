5e67917910bbe9dddcdc7fbc3e77299fac77d0d5
mshelleh <155855025+mshelleh@users.noreply.github.com>
2024-01-11 21:39:11 -0600

    mailingvariables.php

--- mailing/mailingvariables.php
+++ mailing/mailingvariables.php
@@ -1,7 +1,7 @@
 <?php
     $mail_host = "smtp.gmail.com";
     $mail_port = "587";
-    $mail_sender_email = ""; //sender
-    $mail_sender_password = ""; //sender
+    $mail_sender_email = "bytestogo4@gmail.com"; //sender
+    $mail_sender_password = "Saidi@0925"; //sender
     $mail_sender_name = "Website Form";
 ?>
